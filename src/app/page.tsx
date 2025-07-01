dd55010fd8bdfc1648e8ba196b5df541e012a435
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-07-01 09:45:03 -0600

    inciando practica de separacion de ramas esta es la principall

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ const page = () => {
   return (
    <div>
     <h1 className='text-gray-800'>Bienvenido a mundo banda</h1>
-    <h1 className='text-gray-800'>Bienvenido a mundo banda</h1>
-    <h1 className='text-gray-800'>Bienvenido a mundo banda</h1>
+<p>Rama principal</p>
 
 
    </div>
