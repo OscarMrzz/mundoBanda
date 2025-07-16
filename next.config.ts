7d878d67af8b953748b0a07a11aebbe864a3c7ad
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2025-07-16 00:08:57 -0600

    corrigiendo error 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // No supported experimental options specified
+  },
 };
 
 export default nextConfig;
